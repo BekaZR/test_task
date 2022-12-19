b28fe78a6a3eba4edd7c3c721e34b293473f640f
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-19 17:34:33 +0600

    add db models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
+
+class Property(models.Model):
+    key = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.key
+
+
+class Entity(models.Model):
+    modified_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="entities"
+        )
+    value = models.IntegerField()
+    properties = models.ManyToManyField(
+        Property, related_name="entities"
+        )
+
+    def __str__(self):
+        return self.modified_by.username
